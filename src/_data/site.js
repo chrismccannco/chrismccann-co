5b0afb2237d74d60d4a3d90004c2ff646376524e
chrismccannco <chris@chrismccann.co>
2026-06-01 07:46:38 -0500

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -5,7 +5,7 @@ module.exports = {
   defaultOgImage: "https://chrismccann.co/images/og-default.png",
   linkedIn: "https://www.linkedin.com/in/chrismccannco/",
   instagram: "https://www.instagram.com/chrismccann.co/",
-  twitter: "https://x.com/fieldnotescm",
+  twitter: "https://x.com/chrismccanco",
   newsletterArchive: "https://www.linkedin.com/newsletters/field-notes-7050474114494451713/",
   email: "chris@chrismccann.co",
   plausibleScript: "https://plausible.io/js/pa-1yHKOJGHS7zJOVBFS4N-l.js"
